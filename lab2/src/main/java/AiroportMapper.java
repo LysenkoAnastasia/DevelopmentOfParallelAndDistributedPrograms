68d605c48811b95ac520773e4cc5e788a72a3ff9
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 17:43:25 +0300

    Scripted auto-commit on change (2019-10-18 17:43:25) by gitwatch.sh

--- lab2/src/main/java/AiroportMapper.java
+++ lab2/src/main/java/AiroportMapper.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class AiroportMapper extends Mapper<LongWritable, Text, AiroportKey, Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-     
+     AiroportParser airoportParser = new AiroportParser(value.toString());
         String line = value.toString();
         String[] words = line.replaceAll("(?U)[^-\\w\\s]", "").split("\\s+");
         for (String word: words) {
