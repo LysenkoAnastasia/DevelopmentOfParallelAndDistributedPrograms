fea8ae9c6bb1a210a44c2df52500703aeaab8f77
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 22:42:55 +0300

    Scripted auto-commit on change (2019-10-18 22:42:55) by gitwatch.sh

--- lab2/src/main/java/AiroportMapper.java
+++ lab2/src/main/java/AiroportMapper.java
@@ -8,6 +8,6 @@ public class AiroportMapper extends Mapper<LongWritable, Text, AiroportKey, Text
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
      AiroportParser airoportParser = new AiroportParser(value.toString());
 
-    context.write(new AiroportKey(, 0), new Text());
+    context.write(new AiroportKey(getKey(), 0), new Text(getValue()));
     }
 }
