256f78c2243b7f8874c77f9f9bb81eb2ce0fcb22
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 16:41:36 +0300

    Scripted auto-commit on change (2019-10-18 16:41:36) by gitwatch.sh

--- lab2/src/main/java/AiroportKey.java
+++ lab2/src/main/java/AiroportKey.java
@@ -1,3 +1,22 @@
-public class  in {
+import org.apache.hadoop.io.WritableComparable;
 
+public class AiroportKey implements WritableComparable<AiroportKey> {
+    public String value;
+    public int num;
+
+    public AiroportKey() {
+        this.value = value;
+        this.num = 0;
+    }
+
+    public AiroportKey(String value, int num) {
+        this.value = value;
+        this.num = num;
+    }
+
+
+    @Override
+    public int compareTo(AiroportKey airoportKey) {
+        return 0;
+    }
 }
