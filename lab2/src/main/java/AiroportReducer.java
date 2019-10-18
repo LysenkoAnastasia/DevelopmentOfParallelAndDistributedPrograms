d55ef4bc0fd75874d222b13429282d678ef9eefd
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 20:05:37 +0300

    Scripted auto-commit on change (2019-10-18 20:05:37) by gitwatch.sh

--- lab2/src/main/java/AiroportReducer.java
+++ lab2/src/main/java/AiroportReducer.java
@@ -2,5 +2,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class AiroportReducer extends Reducer<AiroportKey, Text, Text, Text> {
+    public void reduce(AiroportKey key, Iterable<> values, Context context) {
+        context.write()
+    }
 
 }
