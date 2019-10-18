7401d9a0254f6733b757a7b0620f6e52bdbeda7e
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 22:55:29 +0300

    Scripted auto-commit on change (2019-10-18 22:55:29) by gitwatch.sh

--- lab2/src/main/java/AiroportReducer.java
+++ lab2/src/main/java/AiroportReducer.java
@@ -1,6 +1,4 @@
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
