1abef3bd56882b81e8b15fad4aa8fdb1212f956b
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 20:19:44 +0300

    Scripted auto-commit on change (2019-10-18 20:19:44) by gitwatch.sh

--- lab2/src/main/java/AiroportReducer.java
+++ lab2/src/main/java/AiroportReducer.java
@@ -1,13 +1,22 @@
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AiroportReducer extends Reducer<AiroportKey, Text, Text, Text> {
     private Text result;
     public void reduce(AiroportKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        
+        long count = 0;
+        Iterator<Text> iter = values.iterator();
+        while(iter.hasNext()) {
+            iter.next();
+            iter.toString();
+            
+            count++;
+        }
         context.write(key, result);
     }
 }
