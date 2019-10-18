d9010362f4ac54a704cc803445645c77584cfe5d
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 17:08:36 +0300

    Scripted auto-commit on change (2019-10-18 17:08:36) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -6,7 +6,12 @@ import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AiroportKey, Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-    
+       
 
+        String line = value.toString();
+        String[] words = line.replaceAll("(?U)[^-\\w\\s]", "").split("\\s+");
+        for (String word: words) {
+            context.write(new AiroportKey(), new Text("1"));
+        }
     }
 }
