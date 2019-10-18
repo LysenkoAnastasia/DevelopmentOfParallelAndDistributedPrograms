ff8ccc625cc1d520a6c6f7462bca16e0544972ea
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 23:45:18 +0300

    Scripted auto-commit on change (2019-10-18 23:45:18) by gitwatch.sh

--- lab2/src/main/java/AiroportPartition.java
+++ lab2/src/main/java/AiroportPartition.java
@@ -1,10 +1,14 @@
-import javafx.scene.text.Text;
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import org.apache.hadoop.mapreduce.Partitioner;
+import org.w3c.dom.Text;
 
-public class AiroportPartition  extends Partitioner< AiroportKey,Text> {
+import java.security.Key;
+
+public class AiriportPartition  extends Partitioner<AiroportKey, Text> {
+    public AiriportPartition() { }
 
     @Override
-    public int getPartition(AiroportKey airoportKey, Text text, int numReduceTasks) {
-        return (airoportKey.hashCode() & Integer.MAX_VALUE) & numReduceTasks;
+    public int getPartition(AiroportKey key, Text value, int numPeduceTasks) {
+        return (key.getAiroportValue().hashCode() & Integer.MAX_VALUE) % numPeduceTasks;
     }
 }
