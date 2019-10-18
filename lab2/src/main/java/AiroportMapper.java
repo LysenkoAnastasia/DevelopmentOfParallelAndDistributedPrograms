56ca02336dc7c09ff2e3117b1e2a1ba402f7dce2
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-18 19:08:48 +0300

    Scripted auto-commit on change (2019-10-18 19:08:48) by gitwatch.sh

--- lab2/src/main/java/AiroportMapper.java
+++ lab2/src/main/java/AiroportMapper.java
@@ -1,4 +1,5 @@
 
+import javafx.util.Pair;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -7,7 +8,7 @@ import java.io.IOException;
 public class AiroportMapper extends Mapper<LongWritable, Text, AiroportKey, Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
      AiroportParser airoportParser = new AiroportParser(value.toString());
-
+        Pair<String, String> pair;
     context.write(new AiroportKey(), new Text());
     }
 }
